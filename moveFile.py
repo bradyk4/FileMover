d152d30b75ebc85cea7b33e2fd32f592737b9644
bradyk4 <bradyk4@mail.uc.edu>
2021-04-23 16:25:32 -0400

    Add classes and methods for file types

--- moveFile.py
+++ moveFile.py
@@ -3,20 +3,32 @@ import os
 import shutil
 from pathlib import Path
 
-image_destDir = "D:\OneDrive\Pictures\Downloaded Pictures";
-txt_destDir = "D:\Downloads\TxtFiles";
-docx_destDir = "D:\Downloads\DocxFiles";
-pptx_destDir = "D:\Downloads\PptxFiles";
-src_dir = Path("D:\Downloads");
+class moveFile:
+    def jpgFiles():
+        src_dir = Path("D:\Downloads");
+        jpg_destDir = "D:\OneDrive\Pictures\Downloaded Pictures";
+        for file_name in src_dir.glob('*.jpg'): # list all jpg files in source directory
+            shutil.move(file_name, image_destDir);
 
-for file_name in src_dir.glob('*.jpg'): # list all jpg files in source directory
-    shutil.move(file_name, image_destDir);
+    def txtFiles():
+        src_dir = Path("D:\Downloads");
+        txt_destDir = "D:\Downloads\TxtFiles";
+        for file_name in src_dir.glob('*.txt'): # list all txt files in source directory
+            shutil.move(file_name, txt_destDir);
 
-for file_name in src_dir.glob(*.txt): # list all txt files in source directory
-    shutil.move(file_name, txt_destDir);
+    def docxFiles():
+        src_dir = Path("D:\Downloads");
+        docx_destDir = "D:\Downloads\DocxFiles";
+        for file_name in src_dir.glob('*.docx'): # list all docx files in source directory
+            shutil.move(file_name, docx_destDir);
 
-for file_name in src_dir.glob(*.docx): # list all docx files in source directory
-    shutil.move(file_name, docx_destDir);
+    def pptxFiles():
+        src_dir = Path("D:\Downloads");
+        pptx_destDir = "D:\Downloads\PptxFiles";
+        for file_name in src_dir.glob('*.pptx'): # list all pptx files in source directory
+            shutil.move(file_name, pptx_destDir);
 
-for file_name in src_dir.glob(*.pptx): # list all pptx files in source directory
-    shutil.move(file_name, pptx_destDir);
+    jpgFiles();
+    txtFiles();
+    docxFiles();
+    pptxFiles();
